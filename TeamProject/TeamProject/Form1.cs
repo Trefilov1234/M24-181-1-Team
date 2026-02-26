0320b0339c49cec9206bc13016239f5ced3b0859
tvropg <timurvaleev.git@gmail.com>
2026-02-26 19:34:38 +0400

    timur lavanda

--- TeamProject/TeamProject/Form1.cs
+++ TeamProject/TeamProject/Form1.cs
@@ -6,5 +6,10 @@ namespace TeamProject
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Тимур Лаваш");
+        }
     }
 }
